2e834c72f7aff53ea19e189343f362a106f1f593
wyy <wuyanyi09@gmail.com>
2014-03-11 23:23:53 +0800

    ci

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # foxriver
 
-
 ## run
+
+### compile
 
 ```
 mkdir build
+cd build
 cmake ..
 make
 ```
+
+### start server
+
+working directory is `foxriver/build`
+
+```
+./bin/foxriver -p 14311
+```
+
+### testing
+
+```
+curl "http://127.0.0.1:14311/?sentence=你好世界"
+```
+
+
 
 ## contact 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 #include "ReqHandler.hpp"
 using namespace FoxRiver;
 
+const char * const USAGE = "usage: %s -p <port>";
+
 int main(int argc, char** argv)
 {
-    int port = 14311;
+    if(argc != 3)
+    {
+        fprintf(stderr, USAGE, argv[0]);
+        return EXIT_FAILURE;
+    }
+
     ReqHandler reqHandler;
-    EpollServer server(port, &reqHandler);
+    EpollServer server(stoi(argv[2]), &reqHandler);
     server.start();
     return EXIT_SUCCESS;
 }
